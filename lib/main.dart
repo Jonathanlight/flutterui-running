c52f31ead942bd4ed63cc42e6f9d0e4a57b3bdd4
jkablan <jkablan@kernix.com>
2019-02-12 15:26:17 +0100

    interface ux done

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutterui Running',
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'Flutterui Running'),
+      color: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: "Wooding Sport",),
     );
   }
 }
@@ -27,21 +28,410 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
   @override
+  void initState(){
+    super.initState();
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: new Text("Alert"),
+          content: new Text("Alert Dialog body"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Close"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        centerTitle: true,
+        toolbarOpacity: 0.5,
+        backgroundColor: Colors.black,
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              icon: const Icon(Icons.menu, color: Colors.white,),
+              onPressed: () { Scaffold.of(context).openDrawer(); },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.add, color: Colors.white,),
+              tooltip: 'Repair it',
+              onPressed: () {},
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: Colors.white,
+        body: ListView(
+      children: <Widget>[
+        Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Container(
+              height: 350.0,
+              width: double.infinity,
             ),
+            Container(
+              height: 200.0,
+              width: double.infinity,
+              decoration: BoxDecoration( 
+                image: new DecorationImage(
+                  image: new AssetImage("assets/runner1.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              top: 125.0,
+              left: 15.0,
+              right: 15.0,
+              child: Material(
+                elevation: 3.0,
+                borderRadius: BorderRadius.circular(7.0),
+                child: Container(
+                  height: 200.0,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(7.0),
+                      color: Colors.white
+                      ,
+                    ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 75.0,
+              left: (MediaQuery.of(context).size.width / 2 - 50.0),
+              child: Container(
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50.0),
+                  image: DecorationImage(
+                        image: AssetImage('assets/runner4.jpg'),
+                        fit: BoxFit.cover
+                      )
+                  ),
+              ),
+            ),
+            Positioned(
+              top: 190.0,
+              left: (MediaQuery.of(context).size.width / 2) - 105.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Jonathan',
+                    style: TextStyle(
+                        fontFamily: 'Comfortaa',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17.0),
+                  ),
+                  SizedBox(height: 7.0),
+                  Text(
+                    'Paris',
+                    style: TextStyle(
+                        fontFamily: 'Comfortaa',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17.0,
+                        color: Colors.grey),
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    children: <Widget>[
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7.0),
+                        ),
+                        color: Colors.green,
+                        onPressed: _showDialog,
+                        child: Text(
+                          'Message',
+                          style: TextStyle(
+                              fontFamily: 'Comfortaa',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15.0,
+                              color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(width: 5.0),
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(7.0),
+                        ),
+                        color: Colors.grey,
+                        onPressed: () {},
+                        child: Text(
+                          'Suivre',
+                          style: TextStyle(
+                              fontFamily: 'Comfortaa',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 15.0,
+                              color: Colors.white),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
           ],
+        ),
+        SizedBox(height: 10.0),
+        Container(
+              height: 200.0,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: new DecorationImage(
+                  image: new AssetImage("assets/runner1.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(1),
+                    child: Text("Lorem ipsum"),
+                  ),
+                ],
+              )
+        ),
+        SizedBox(height: 10.0),
+        Padding(
+          padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Programme',
+                style: TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '...',
+                style: TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontSize: 15.0,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w300),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 10.0),
+        Padding(
+          padding: EdgeInsets.only(left: 15.0, right: 5.0),
+          child: Container(
+            height: 125.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                getWorks('assets/runner3.jpg'),
+                getWorks('assets/runner4.jpg'),
+                getWorks('assets/runner5.jpg'),
+                getWorks('assets/runner6.jpg'),
+                getWorks('assets/runner7.jpg'),
+                getWorks('assets/runner3.jpg'),
+                getWorks('assets/runner4.jpg'),
+                getWorks('assets/runner5.jpg'),
+                getWorks('assets/runner6.jpg'),
+                getWorks('assets/runner7.jpg'),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: 15.0),
+        Padding(
+          padding: EdgeInsets.only(left: 15.0, right: 15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'Stories',
+                style: TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '___',
+                style: TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontSize: 15.0,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w300),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 10.0),
+        Column(
+          children: <Widget>[
+            menuCard('Running london', 'assets/runner1.jpg','Best running of Paris', 2, 2.8, 1.2),
+            SizedBox(height: 12.0),
+            menuCard('Running paris', 'assets/runner2.jpg', 'Best running of Paris', 4, 4.2, 2.8),
+            SizedBox(height: 12.0),
+            menuCard('Running paris', 'assets/runner3.jpg', 'Best running of Paris', 5, 4.2, 2.8),
+            SizedBox(height: 12.0),
+            menuCard('Running paris', 'assets/runner4.jpg', 'Best running of Paris', 4, 4.2, 2.8),
+            SizedBox(height: 12.0),
+            menuCard('Running paris', 'assets/runner5.jpg', 'Best running of Paris', 3, 4.2, 2.8),
+            SizedBox(height: 12.0),
+            menuCard('Running paris', 'assets/runner6.jpg', 'Best running of Paris', 4, 4.2, 2.8),
+          ],
+        ),
+        SizedBox(height: 25.0),
+        Padding(
+          padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                'More',
+                style: TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Register',
+                style: TextStyle(
+                    fontFamily: 'Comfortaa',
+                    fontSize: 15.0,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.w300),
+              )
+            ],
+          ),
+        ),
+        SizedBox(height: 15.0),
+      ],
+    ));
+  }
+
+  Widget getWorks(String imgPath) {
+    return Padding(
+      padding: EdgeInsets.only(right: 10.0),
+      child: Container(
+        height: 100.0,
+        width: 125.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(7.0),
+          image: DecorationImage(
+            image: AssetImage(imgPath),
+            fit: BoxFit.cover
+          )
         ),
       ),
     );
+  }
+
+  Widget menuCard(String title, String imgPath, String type, int rating,
+      double views, double likes) {
+    return Padding(
+      padding: EdgeInsets.only(left: 10.0, right: 10.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(7.0),
+        elevation: 4.0,
+        child: Container(
+          height: 125.0,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(7.0), color: Colors.white),
+          child: Row(
+            children: <Widget>[
+              SizedBox(width: 10.0),
+              Container(
+                height: 100.0,
+                width: 100.0,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(imgPath), fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(7.0)),
+              ),
+              SizedBox(width: 10.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(height: 15.0),
+                  Text(
+                    title,
+                    style: TextStyle(
+                        fontFamily: 'Comfortaa',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 7.0),
+                  Text(
+                    type,
+                    style: TextStyle(
+                        fontFamily: 'Comfortaa',
+                        color: Colors.grey,
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w400),
+                  ),
+                  SizedBox(height: 7.0),
+                  Row(
+                    children: <Widget>[
+                      getStar(rating, 1),
+                      getStar(rating, 2),
+                      getStar(rating, 3),
+                      getStar(rating, 4),
+                      getStar(rating, 5)
+                    ],
+                  ),
+                  SizedBox(height: 4.0),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.remove_red_eye,
+                          color: Colors.grey.withOpacity(0.4)),
+                      SizedBox(width: 3.0),
+                      Text(views.toString()),
+                      SizedBox(width: 10.0),
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      ),
+                      SizedBox(width: 3.0),
+                      Text(likes.toString())
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getStar(rating, index) {
+    if (rating >= index) {
+      return Icon(Icons.star, color: Colors.red);
+    } else {
+      return Icon(Icons.star, color: Colors.grey.withOpacity(0.5));
+    }
   }
 }
